21aefb6262890d68ee68d05dfd40212c055045f4
fojobass <fojo4god@gmail.com>
2024-07-24 10:00:59 +0100

    error fix

--- server/src/Utils/OutputResolver.php
+++ server/src/Utils/OutputResolver.php
@@ -11,7 +11,7 @@ final class OutputResolver
  public static function resolver(callable $resolveFunc, array $arg = [], bool $isArgArray = false)
  {
   try {
-   $args[0] = $arg[0];
+   $args[0] = $arg[0] ?? 0;
    $args[1] = $isArgArray;
    return $isArgArray ? call_user_func_array($resolveFunc, $args)  : call_user_func($resolveFunc, $arg[0] ?? 0);
   } catch (Throwable $e) {
