c3d86c484cf441953537290799a4b560a34fcad4
fojobass <fojo4god@gmail.com>
2024-07-25 12:12:49 +0100

    And Another CORS fix

--- server/api/index.php
+++ server/api/index.php
@@ -34,7 +34,7 @@ switch ($routeInfo[0]) {
     break;
 }
 
-header('Content-Type: application/json');
+header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Headers: *");
